7728974a0fc40bea8ab05e0f60d6e8c23c0aa4f6
Shivam Abrol <motoeshivam@gmail.com>
2023-03-16 03:46:27 -0400

    focus + context works not total

--- js/index.js
+++ js/index.js
@@ -575,141 +575,10 @@ function clicked(event, d) {
 }
 
 //code for focus + context
-function plotfocusTimeline() {
-  var margin = { top: 10, right: 10, bottom: 100, left: 40 },
-    margin2 = { top: 430, right: 10, bottom: 20, left: 40 },
-    width = 800 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom,
-    height2 = 500 - margin2.top - margin2.bottom;
 
-  var parseDate = d3.timeParse("%Y");
+function plotFocusTimeline() {}
 
-  var x = d3.scaleUtc().range([0, width]),
-    x2 = d3.scaleUtc().range([0, width]),
-    y = d3.scaleLinear().range([height, 0]),
-    y2 = d3.scaleLinear().range([height2, 0]);
-
-  var xAxis = d3.axisBottom(x),
-    xAxis2 = d3.axisBottom(x2),
-    yAxis = d3.axisLeft(y);
-
-  var brush = d3
-    .brushX()
-    .extent([
-      [0, 0],
-      [width, height2],
-    ])
-    .on("brush", brushed);
-
-  var area = d3
-    .area()
-    .curve(d3.curveMonotoneX)
-    .x(function (d) {
-      return x(parseInt(d["timestamp"]));
-    })
-    .y0(height)
-    .y1(function (d) {
-      return y(parseInt(d["location-lat"]));
-    });
-
-  var area2 = d3
-    .area()
-    .curve(d3.curveMonotoneX)
-    .x(function (d) {
-      return x2(parseInt(d["timestamp"]));
-    })
-    .y0(height2)
-    .y1(function (d) {
-      return y2(parseInt(d["location-lat"]));
-    });
-
-  // var svg = d3
-  //   .select("body")
-  //   .append("svg")
-  //   .attr("width", width + margin.left + margin.right)
-  //   .attr("height", height + margin.top + margin.bottom);
-
-  // append the svg object to the body of the page
-  const svg = d3
-    .select("#my_dataviz")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
-
-  svg
-    .append("defs")
-    .append("clipPath")
-    .attr("id", "clip")
-    .append("rect")
-    .attr("width", width)
-    .attr("height", height);
-
-  var focus = svg
-    .append("g")
-    .attr("class", "focus")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  var context = svg
-    .append("g")
-    .attr("class", "context")
-    .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
-
-  // if (error) throw error;
-
-  x.domain(
-    d3.extent(
-      data.map(function (d) {
-        return d["timestamp"];
-      })
-    )
-  );
-  y.domain([
-    0,
-    d3.max(
-      data.map(function (d) {
-        return parseInt(d["location-lat"]);
-      })
-    ),
-  ]);
-  x2.domain(x.domain());
-  y2.domain(y.domain());
-
-  focus.append("path").datum(data).attr("class", "area").attr("d", area);
-
-  focus
-    .append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis);
-
-  focus.append("g").attr("class", "y axis").call(yAxis);
-
-  context.append("path").datum(data).attr("class", "area").attr("d", area2);
-
-  context
-    .append("g")
-    .attr("class", "x axis")
-    .attr("transform", `translate(0,${height2})`)
-    .call(xAxis2);
-
-  context
-    .append("g")
-    .attr("class", "x brush")
-    .call(brush)
-    .selectAll("rect")
-    .attr("y", -6)
-    .attr("height", height2 + 7);
-  function brushed(event) {
-    x.domain(event ? event.selection : x2.domain());
-    focus.select(".area").attr("d", area);
-    focus.select(".x.axis").call(xAxis);
-  }
-}
-
-//developing the trajectory plotter while this is in progress
-plotfocusTimeline();
+plotFocusTimeline();
 
 function isolatePoints() {
   const circles = d3.selectAll("circle.points");
@@ -980,3 +849,123 @@ function selectAll(id) {
     d3.selectAll("circle.points").remove();
   }
 }
+
+let data1;
+d3.csv("../data/Dead-Reackon-Sample-1.csv").then(function (data) {
+  data1 = data;
+  const margin = { top: 10, right: 10, bottom: 100, left: 40 };
+  const margin2 = { top: 430, right: 10, bottom: 20, left: 40 };
+  const width = 960 - margin.left - margin.right;
+  const height = 500 - margin.top - margin.bottom;
+  const height2 = 500 - margin2.top - margin2.bottom;
+
+  //%b
+
+  const x = d3.scaleTime().range([0, width]);
+  const x2 = d3.scaleTime().range([0, width]);
+  const y = d3.scaleLinear().range([height, 0]);
+  const y2 = d3.scaleLinear().range([height2, 0]);
+
+  const xAxis = d3.axisBottom(x);
+  const xAxis2 = d3.axisBottom(x2);
+  const yAxis = d3.axisLeft(y);
+
+  const brush = d3
+    .brushX()
+    .extent([
+      [0, 0],
+      [width, height2],
+    ])
+    .on("brush end", brushed);
+
+  // create the area
+  const area = d3
+    .area()
+    .x(function (d) {
+      return x(new Date(d["date_string"]));
+    })
+    .y0(height)
+    .y1(function (d) {
+      return y(d["trajectory-finder"]);
+    });
+
+  // create the area
+  const area2 = d3
+    .area()
+    .x(function (d) {
+      return x2(new Date(d["date_string"]));
+    })
+    .y0(height)
+    .y1(function (d) {
+      return y2(d["trajectory-finder"]);
+    });
+
+  // var lineV = d3.svg.line()
+  //                   .interpolate("monotone")
+  //                   .x(function(d) { return x(d.date); })
+  //                   .y(function(d) { return yV(d.y)});
+
+  const svg2 = d3
+    .select("my_dataviz")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom);
+
+  svg2
+    .append("defs")
+    .append("clipPath")
+    .attr("id", "clip")
+    .append("rect")
+    .attr("width", width)
+    .attr("height", height);
+
+  const focus = svg2
+    .append("g")
+    .attr("class", "focus")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  const context = svg2
+    .append("g")
+    .attr("class", "context")
+    .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
+
+  x.domain([new Date(2022, 6, 1), new Date(2022, 10, 1)]);
+  y.domain([0, 100]);
+  x2.domain(x.domain());
+  y2.domain(y.domain());
+
+  focus.append("path").attr("class", "area").attr("d", area(data));
+
+  focus
+    .append("g")
+    .attr("class", "x axis")
+    .attr("transform", `translate(0,${height})`)
+    .call(xAxis);
+
+  focus.append("g").attr("class", "y axis").call(yAxis);
+
+  context.append("path").attr("class", "area").attr("d", area2(data));
+
+  context
+    .append("g")
+    .attr("class", "x axis")
+    .attr("transform", `translate(0,${height2})`)
+    .call(xAxis2);
+
+  context
+    .append("g")
+    .attr("class", "x brush")
+    .call(brush)
+    .selectAll("rect")
+    .attr("y", -6)
+    .attr("height", height2 + 7);
+
+  function brushed(event) {
+    if (event.selection) {
+      const [x0, x1] = event.selection.map(x2.invert);
+      x.domain([x0, x1]);
+      focus.select(".area").attr("d", area(data1));
+      focus.select(".x.axis").call(xAxis);
+    }
+  }
+});
